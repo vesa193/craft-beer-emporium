3b4bb99a9dfcf6c85019360628cf9d31cf69c1be
Lazar Veselinovic <lazar.veselinovic@centific.com>
2024-09-26 00:51:30 +0200

    feat: add useFetch custom hook, initial list beers view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { useFetch } from '../hooks/useFetch';
+
+type TBeer = {
+    id: number;
+    price: string;
+    name: string;
+    rating: {
+        average: number;
+        reviews: number;
+    };
+    image: string;
+};
 
 const Home = () => {
-    return <h1>Home page</h1>;
+    const { data } = useFetch(import.meta.env.VITE_API_URL);
+    const { fetchedData, isLoading, error } = data;
+    console.log(data);
+
+    if (isLoading) {
+        return <p>Loading ...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    return (
+        <>
+            <h1>Home page</h1>
+            <br />
+            <br />
+            <ul>
+                {(fetchedData as TBeer[])?.map(({ id, name, image }) => {
+                    return (
+                        <li key={id}>
+                            {name}
+                            {image && <img src={image} alt={name} />}
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
 };
 
 export default Home;
