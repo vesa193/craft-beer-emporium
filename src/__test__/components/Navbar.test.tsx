3ba2edac816a8a165f04c92abc872371e70eb71f
Lazar Veselinovic <lazar.veselinovic@centific.com>
2024-09-30 23:39:18 +0200

    lint: remove unused imports

--- src/__test__/components/Navbar.test.tsx
+++ src/__test__/components/Navbar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { MemoryRouter } from 'react-router';
 import { Navbar } from '../../components/Navbar';
